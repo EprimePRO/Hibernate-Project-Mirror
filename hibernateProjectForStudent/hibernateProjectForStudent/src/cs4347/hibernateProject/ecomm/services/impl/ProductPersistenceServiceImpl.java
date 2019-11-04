8fc1e67a343135fc2f90066b0fd1c2e540f35af1
Ray <54683014+raymaster7@users.noreply.github.com>
2019-11-03 21:01:11 -0600

    can't get both retrieves to work

--- hibernateProjectForStudent/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
+++ hibernateProjectForStudent/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
@@ -17,6 +17,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import cs4347.hibernateProject.ecomm.entity.Customer;
 import cs4347.hibernateProject.ecomm.entity.Product;
 import cs4347.hibernateProject.ecomm.services.ProductPersistenceService;
 import cs4347.hibernateProject.ecomm.util.DAOException;
@@ -33,34 +34,87 @@ public class ProductPersistenceServiceImpl implements ProductPersistenceService
 	@Override
 	public void create(Product product) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			em.persist(product);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public Product retrieve(Long id) throws SQLException, DAOException
 	{
-		return null;
+		try {
+			em.getTransaction().begin();
+			Product pro = em.find(Product.class, id);
+			em.getTransaction().commit();
+			return pro;
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public void update(Product product) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			Product p2 = em.find(Product.class, product.getId());
+			p2.setProdUPC(product.getProdUPC());
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public void delete(Long id) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			Product c2 = em.find(Product.class, id);
+			em.remove(c2);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public Product retrieveByUPC(String upc) throws SQLException, DAOException
 	{
-		return null;
+		em.getTransaction().begin();
+		Product rupc = (Product)em.createQuery("from Product as p where p.upc = :upc")
+				.setParameter("upc", upc)
+				.getSingleResult();
+		em.getTransaction().commit();
+		
+		if(rupc == null) {
+			throw new DAOException("UPC Not Found " + upc);
+		}
+		
+		return rupc;
 	}
 
 	@Override
 	public List<Product> retrieveByCategory(int category) throws SQLException, DAOException
 	{
-		return null;
+		em.getTransaction().begin();
+		List<Product> emps = (List<Product>)em.createQuery("from Product where category = :category")
+			.setParameter("category", category)
+			.getResultList();
+		em.getTransaction().commit();
+		return emps;
 	}
 
 }
