f66f486191fbe3951d9cf690ae53b7e2e1a917e1
Ray <54683014+raymaster7@users.noreply.github.com>
2019-11-03 20:11:11 -0600

    finished and works

--- hibernateProjectForStudent/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/CustomerPersistenceServiceImpl.java
+++ hibernateProjectForStudent/hibernateProjectForStudent/src/cs4347/hibernateProject/ecomm/services/impl/CustomerPersistenceServiceImpl.java
@@ -19,6 +19,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import cs4347.hibernateProject.ecomm.entity.Customer;
+import cs4347.hibernateProject.ecomm.entity.Purchase;
 import cs4347.hibernateProject.ecomm.services.CustomerPersistenceService;
 import cs4347.hibernateProject.ecomm.util.DAOException;
 
@@ -36,33 +37,85 @@ public class CustomerPersistenceServiceImpl implements CustomerPersistenceServic
 	@Override
 	public void create(Customer customer) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			em.persist(customer);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public Customer retrieve(Long id) 
 	{
-		return null;
+		try {
+		em.getTransaction().begin();
+		Customer cus = em.find(Customer.class, id);
+		em.getTransaction().commit();
+		return cus;
+	}
+	catch (Exception ex) {
+		em.getTransaction().rollback();
+		throw ex;
+	}
 	}
 
 	@Override
 	public void update(Customer c1) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			Customer c2 = em.find(Customer.class, c1.getId());
+			c2.setAddress(c1.getAddress());
+			c2.setCreditCard(c1.getCreditCard());
+			c2.setEmail(c1.getEmail());
+			//c2.setDob(c1.getDob());
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public void delete(Long id) throws SQLException, DAOException
 	{
+		try {
+			em.getTransaction().begin();
+			Customer c2 = em.find(Customer.class, id);
+			em.remove(c2);
+			em.getTransaction().commit();
+		}
+		catch (Exception ex) {
+			em.getTransaction().rollback();
+			throw ex;
+		}
 	}
 
 	@Override
 	public List<Customer> retrieveByZipCode(String zipCode) throws SQLException, DAOException
 	{
-		return null;
+		em.getTransaction().begin();
+		List<Customer> emps = (List<Customer>)em.createQuery("from Customer as c where c.address.zipcode = :zip")
+			.setParameter("zip", zipCode)
+			.getResultList();
+		em.getTransaction().commit();
+		return emps;
 	}
 
 	@Override
 	public List<Customer> retrieveByDOB(Date startDate, Date endDate) throws SQLException, DAOException
 	{
-		return null;
+		em.getTransaction().begin();
+		List<Customer> emps = (List<Customer>)em.createQuery("from Customer as c where c.dob between :start and :end")
+			.setParameter("start", startDate)
+			.setParameter("end", endDate)
+			.getResultList();
+		em.getTransaction().commit();
+		return emps;
 	}
 }
